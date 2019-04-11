34906d2b3ab10639f978930ec51396beedd4317f
Dakota Clark <dakotaclark92@yahoo.com>
2019-04-11 03:32:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,3 @@
-
-require_relative '../../config/environment'
-
-class ApplicationController < Sinatra::Base
-
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
-
-  get '/' do
-  end
-end
-
-
-require_relative '../../config/environment'
-
 class ApplicationController < Sinatra::Base
 
   configure do
