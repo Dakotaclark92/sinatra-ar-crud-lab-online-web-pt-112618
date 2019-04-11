1b1f958a73cc23a26e657b4222c520c97871b154
Dakota Clark <dakotaclark92@yahoo.com>
2019-04-11 03:26:40 +0000

    Automatically backed up by Learn

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,23 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+  attr_reader :title, :content
+
+  ARTICLES = []
+  
+  def self.all
+    @@all
+  end
+  
+  def initialize(params)
+    @title = params[:title]
+    @content = params[:content]
+    ARTICLES << self
+  end
+  
+  def self.all
+    ARTICLES
+  end
+
+  
 end
