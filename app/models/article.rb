498dc73e14ccd1bdad7131fba734392d8300b483
Dakota Clark <dakotaclark92@yahoo.com>
2019-04-11 03:46:10 +0000

    Automatically backed up by Learn

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,22 +2,5 @@
 
 class Article < ActiveRecord::Base
   attr_reader :title, :content
-
-  ARTICLES = []
-  
-  def self.all
-    @@all
-  end
-  
-  def initialize(params)
-    @title = params[:title]
-    @content = params[:content]
-    ARTICLES << self
-  end
-  
-  def self.all
-    ARTICLES
-  end
-
   
 end
